1963007ff08e651a104a6e2b57918bfcf0d29d74
Alexis-lijeron <alexislijeron.a@gmail.com>
2025-05-19 20:13:30 -0400

    seed DocenteMateria

--- seeders/run_seed.py
+++ seeders/run_seed.py
@@ -3,15 +3,18 @@ from seeders.seed_cursos import seed_cursos
 from seeders.seed_materias import seed_materias
 from seeders.seed_curso_materia import seed_curso_materia
 from seeders.seed_docentes import seed_docentes
+from seeders.seed_docente_materia import seed_docente_materia
+
 
 def run():
     db = SessionLocal()
-    #seed_materias(db)
-    seed_docentes(db)
-    #seed_gestion(db)
-    #seed_cursos(db)
-   # seed_curso_materia(db)
+    # seed_materias(db)
+    # seed_docentes(db)
+    # seed_gestion(db)
+    # seed_cursos(db)
+    # seed_curso_materia(db)
     # seed_estudiantes(db)
+    seed_docente_materia(db)
     db.close()
 
 
