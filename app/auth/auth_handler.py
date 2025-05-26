269ac34611d2d7e7004627e5011dac777c644822
Alexis-lijeron <alexislijeron.a@gmail.com>
2025-05-26 16:48:23 -0400

    sin limite

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -8,11 +8,12 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY", "secret123")
 ALGORITHM = "HS256"
 EXPIRE_MINUTES = 60
+EXPIRE_YEARS = 10
 
 
 def crear_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(days=365 * EXPIRE_YEARS)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
